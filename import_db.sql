a957ba2b8cb3d82f4fdf239fc53ee895bbe75c7b
Aline Tavares <alaisgomes@gmail.com>
2017-01-12 13:48:30 -0200

    Arquivo final do banco

---
+++ import_db.sql
@@ -0,0 +1,45 @@
+
+DROP TABLE IF EXISTS Projeto;
+
+CREATE TABLE Projeto
+(
+	idProjeto int NOT NULL, 
+	nome VARCHAR(200),
+	PRIMARY KEY (idProjeto)
+
+);
+
+
+DROP TABLE IF EXISTS Projeto;
+
+CREATE TABLE Versao
+(
+	idVersao int NOT NULL,
+	idProjeto int NOT NULL,
+	nome VARCHAR(200),
+	PRIMARY KEY (idVersao),
+	FOREIGN KEY (idProjeto) REFERENCES Projeto(idProjeto)
+);
+
+CREATE TABLE Classe
+(
+	idClasse int NOT NULL,
+	idProjeto int NOT NULL,
+	nome VARCHAR(200),
+	PRIMARY KEY (idClasse),
+	FOREIGN KEY (idProjeto) REFERENCES Projeto(idProjeto)
+);
+
+CREATE TABLE Metodo
+(
+	idMetodo int NOT NULL,
+	idClasse int NOT NULL,
+	nome VARCHAR(200),
+	QtdLambda int,
+	QtdForEach int,
+	QtdAic int,
+	QtdFilter int,
+	QtdMaps int,
+	PRIMARY KEY (idMetodo),
+	FOREIGN KEY (idClasse) REFERENCES Classe(idClasse)
+);
