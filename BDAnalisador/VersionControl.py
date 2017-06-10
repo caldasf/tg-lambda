667f23241c8e92217bc063b4039da350c0d2aa72
Aline Tavares <alaisgomes@gmail.com>
2017-06-10 15:56:46 -0300

    Script static-analysis atualizado.

--- BDAnalisador/VersionControl.py
+++ BDAnalisador/VersionControl.py
@@ -13,7 +13,7 @@ import CreateInput
 fname = os.getcwd() + '/input_temp.csv';
 
 #preference folder where will save new inputs for static-analysis
-inputs_location = os.getcwd() + '/inputs/';
+inputs_location = os.getcwd() + '/input/';
 
 #directory where all projects are
 projects_dir = '/home/luna/TG/java-projects/'
@@ -28,72 +28,63 @@ BDAnalisador = os.getcwd() + '/BDAnalisador.jar'
 
 ############################################################################
 
-def runBDAnalisador(inputFile):
-	os.system('java -jar ' + BDAnalisador)
+def runBDAnalisador(month_input_path):
+	os.system('java -jar {}'.format(BDAnalisador))
 
 
-def runStaticAnalysis(inputFile):
-	os.system('java -jar ' + static_jar +' '+inputFile)
+def run_static_analysis(month_input_path):
+	os.system('java -jar {} {}'.format(static_jar, month_input_path))
 
 
-def createOneInput(newInput):
+def add_final_input(month_input):
 	output = open (final_input, 'a')
-	with open(newInput, "r") as bFile:
+
+	with open(month_input, "r") as bFile:
 		projects = csv.reader(bFile, delimiter=";")
 		for row in projects:
-			output.write(row[0] + ';' + row[1] + ';' + row[2] + ';' + row[3] + ';' + row[4] + ';' + row[5] + ';' + row[6] + '\n' )
+			output.write("{};{};{};{};{};{};{};\n".format(row[0], 
+				row[1], row[2], row[3], row[4], row[5],row[6]))			
+	
 	output.close()
 
 
 
-def getProject(project, slocF):
-	aFile = file(slocF, "r")
+def find_project(project_name_key, sloc_fp):
+	aFile = file(sloc_fp, "r")
 	reader = csv.reader(aFile)
 	for row in reader:
 		projectName = row[0][:-4]
-		if (projectName == project):
+		if (projectName == project_name_key):
 			return row[1]
 	aFile.close()
 
+	return False
 
-def changeLineCount(inputFile, tempFile):
-	slocFile = CreateInput.countLinesProjects(projects_dir, 'summary.csv')
-	#slocFile = '/home/les-02/TGLambda/tg-lambda/BDAnalisador/summary.csv'
+
+def count_lines_code(month_input_path, temp_input_path):
+	sloc_file_path = CreateInput.countLinesProjects(projects_dir, 'summary.csv')
 	vet = [0 for i in xrange(500)]
 	i = 0
-	 
-
-	#open slocFile
-	#aFile = file(slocFile, "r")
-	#reader = csv.reader(aFile)
 	
-	with open(tempFile, "r") as bFile:
-		
+	with open(temp_input_path, "r") as bFile:		
 		projects = csv.reader(bFile, delimiter=";")
 		for project in projects:
-			vet[i] = getProject(project[2], slocFile)
-			#print vet[i]
+			vet[i] = find_project(project[2], sloc_file_path)
 			i += 1
-						
-					
-
 	bFile.close() 	
  	
 	i = 0
-	with open(tempFile, "r") as bFile:
+	with open(temp_input_path, "r") as bFile:
 		projects = csv.reader(bFile, delimiter=";")
 		
- 		output = open (inputFile, 'w')
- 		string_linhas = str(vet[i])
+ 		output = open (month_input_path, 'w')
 
  		for project in projects:
-			#print string_linhas
-			#print vet[i]
- 			output.write(project[0] + ';' + project[1] + ';' + project[2]+ ';' + project[3]+ ';' + project[4]+ ';' + str(vet[i]) +';' + project[6] + ';' + '\n');
+ 			output.write("{};{};{};{};{};{};{};\n".format(project[0], 
+ 				project[1], project[2],project[3], project[4], vet[i], project[6]))
  			i += 1
 
  	bFile.close() 
-
  	output.close()
 
 
@@ -102,85 +93,68 @@ def is_non_zero_file(fpath):
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
 
 
-
-def changeInput(date_after, date_before):
-	newInput = inputs_location + 'input-' + date_after + '_' + date_before + '.csv';
-
-	arq_temp = os.getcwd() + "/temp1.csv"
+def change_input(date_after, date_before):
+	month_input = inputs_location + 'input-' + date_after + '_' + date_before + '.csv';
+	temp_input_path = os.getcwd() + "/temp1.csv"
 
 	with open(fname, "r") as aFile:
 		projects = csv.reader(aFile, delimiter=";")
 	
-		cwd = os.getcwd()
+		working_dir_saved = os.getcwd()
 
-		newFile = open(arq_temp, 'w');
+		temp_input_file = open(temp_input_path, 'w') # temp file for sloc
 
 		for project in projects:
-			
-			hash_version = ''
 			path_project = project[4]
 
 			#change working directory to project directory
 			os.chdir(path_project)
-
-			os.system('git log --after="' + date_after + '" --before="' + date_before +'" -1 --date=format:"%Y-%m-%d" --pretty=format:"%H, %an, %cd, %s" > temp.csv')
+			os.system('git log --after="{}" --before="{}" -1 \
+				--date=format:"%Y-%m-%d" --pretty=format:"%H, %an, %cd, %s" > temp.csv\
+				'.format(date_after, date_before))
 			
-
-			#if log is not empty = exists a commit in this period
-   
+			#if log is not empty = exists a commit in this period   
 			if (is_non_zero_file(path_project + 'temp.csv')):
 
 				f = open('temp.csv', 'rt')
-		  		temp = csv.reader(f)
+		  		git_temp_rows = csv.reader(f) # result from the git command
 
 
-				for row in temp:
-					project[3] = row[0]
+				for row in git_temp_rows:
+					project[3] = row[0] #
 
-				#save new row in the new file
-				newFile.write(project[0] + ';' + project[1] + ';' + project[2]+ ';' + project[3]+ ';' + project[4]+ ';' + project[5] + ';' + row[2] + '\n');
-
+				temp_input_file.write("{};{};{};{};{};{};{};\n".format(project[0], 
+					project[1], project[2], project[3], project[4], project[5], row[2]))
+			
 				f.close()
 
 				#change version of repository
-				os.system('git checkout '+ project[3] + ' .')
+				#os.system('git checkout '+ project[3] + ' .')
+				os.system("rm -r *")
+				os.system("git archive --format=tar  {} | tar xf -".format(project[3]))
 
 		aFile.close()
 
-		#remove(path_project + 'temp.csv')
-
-	#return to previous working directory
-	os.chdir(cwd)
-	newFile.close()	
+	os.chdir(working_dir_saved)
+	temp_input_file.close()	
 
 
-	#
+	count_lines_code(month_input, temp_input_path)
 
-	#change line count in the new input file
-	changeLineCount(newInput, arq_temp)
+	run_static_analysis(month_input)
 
-	#run static-analysis in new input file
-	runStaticAnalysis(newInput)
-
-	#append to final input.csv file
-	createOneInput(newInput)
+	add_final_input(month_input)
 
 
 
-def main ():
-
-
-	date_after = '2017-1-1'
-	date_before = '2017-2-1'
-
+def main():
 	day = 01
-	month = 04
+	month = 06
 	year = 2017
 
 	while (year > 2012):
 
 		date_before = str(year) + '-' + str(month) + '-' + str(day)
-
 
 		if (month > 1):
 			month -= 1
@@ -188,22 +162,21 @@ def main ():
 			month = 12
 			year -= 1
 
-		date_after = str(year) + '-' + str(month) + '-' + str(day)
-		
-		print date_after + ' & ' + date_before
-		changeInput(date_after, date_before)
+		date_after = str(year) + '-' + str(month) + '-' + str(day)		
+		print("after {} & before {}".format(date_after, date_before))
+
+		change_input(date_after, date_before)
 	
-	runBDAnalisador(final_input)
+	# runBDAnalisador(final_input)
 
 
 
 ############################################################
 
-#output = open (final_input, 'w')
-#output.close()
+output = open (final_input, 'w')
+output.close()
 
 #fname = CreateInput.main()
 
-main()
-
-#changeLineCount('test.csv', 'input_temp.csv')
+if __name__ == '__main__':
+	main()
